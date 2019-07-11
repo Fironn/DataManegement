cbcea3eb1fd793a45f84a142a179a5c159e7af23
Fironn <shihomihashi@gmail.com>
2019-07-11 22:25:44 +0900

    [ADD]more feature

--- dice/app.py
+++ dice/app.py
@@ -3,12 +3,13 @@ import random
 def main(name):
     x=int(random.uniform(1,6))
     y=int(random.uniform(1,6))
+    z=int(random.uniform(1,6))
 
     print("Die 1: "+str(x))
     print("Die 2: "+str(y))
+    print("Die 3: "+str(z))
 
-    total=x+y
-
+    total=x+y+z
     print("Total value: "+str(total))
 
     if total > 7:
@@ -16,6 +17,17 @@ def main(name):
     else:
         print(name+" lost!")
 
+    print("You get "+fizzBuzz(total))
+
+def fizzBuzz(num):
+    if num%3==0 and num%5==0:
+        return "FizzBuzz"
+    elif num%3==0:
+        return "Fizz"
+    elif num%5==0:
+        return "Buzz"
+    else:
+        return str(num)
 
 def askName():
     name=input("What is your name?\n> ")
